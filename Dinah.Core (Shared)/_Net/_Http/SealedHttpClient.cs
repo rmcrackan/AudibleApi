62dfe3896bb66bd82adeadc6a1ebcbfa88398d83
Robert McRackan <rmcracka@itron.com>
2019-09-19 14:54:58 -0400

    yet another re-org. Shared code is no longer sym-linked. Too many errors. git submodules was also a bust. going back to good old visual studio references. Common code is in Dinah.Core repo

--- Dinah.Core (Shared)/_Net/_Http/SealedHttpClient.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Net.Http;
-
-namespace Dinah.Core.Net.Http
-{
-	public class SealedHttpClient : HttpClient, ISealedHttpClient
-	{
-		public SealedHttpClient(HttpMessageHandler handler) : base(handler) { }
-	}
-}
