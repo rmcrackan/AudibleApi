e3958b44c817946e2ffe3467b84f6ac6cbed9c11
Robert McRackan <rmcracka@itron.com>
2021-07-11 09:36:25 -0400

    Bug fix: cvf/apporval-needed html recently changed

--- AudibleApi/Authentication/ResultFactory.ApprovalNeededPageFactory.cs
+++ AudibleApi/Authentication/ResultFactory.ApprovalNeededPageFactory.cs
@@ -14,7 +14,9 @@ namespace AudibleApi.Authentication
             public ApprovalNeededPageFactory() : base(nameof(ApprovalNeededPageFactory)) { }
 
             protected override bool _isMatchAsync(HttpResponseMessage response, string body)
-                => HtmlHelper.GetDivCount(body, "resend-approval-alert") > 0;
+                => HtmlHelper.GetDivCount(body, "resend-approval-alert") > 0
+                || HtmlHelper.GetElements(body, "form", "id", "resend-approval-form").Any()
+                || HtmlHelper.GetElements(body, "a", "id", "resend-approval-link").Any();
 
             protected override LoginResult _createResultAsync(Authenticate authenticate, HttpResponseMessage response, string body, Dictionary<string, string> oldInputs)
                 => new ApprovalNeededPage(authenticate, body);
