99f00d3167905d56609b4347f7622868e60e5043
Mbucari <37587114+Mbucari@users.noreply.github.com>
2025-11-30 10:50:18 -0700

    Improve error logging in GetActivationBytesAsync

--- AudibleApi/Api.Activation.cs
+++ AudibleApi/Api.Activation.cs
@@ -3,6 +3,7 @@ using System;
 using System.Text;
 using System.Threading.Tasks;
 
+#nullable enable
 namespace AudibleApi
 {
     public partial class Api
@@ -18,12 +19,18 @@ namespace AudibleApi
             // note: this call uses the audible login uri, NOT api
             var client = Sharer.GetSharedHttpClient(Locale.AudibleLoginUri());
 
-            var response = await AdHocAuthenticatedGetAsync($"/license/token?action=register&player_manuf=Audible,iPhone&player_model=iPhone", client);
+            var response = await AdHocAuthenticatedGetAsync($"/license/token?action=register&player_manuf=Audible,Android&player_model=Android", client);
 
             var deviceLicense = await response.Content.ReadAsByteArrayAsync();
 
             if (deviceLicense.Length < ACTIVATION_BLOB_SZ)
-                throw new ApiErrorException(response.Headers.Location, new JObject { { "error", "Unexpected activation response: " + await response.Content.ReadAsStringAsync() } });
+                throw new ApiErrorException(response.Headers.Location, new JObject
+                {
+                    { "error", "Unexpected activation response" },
+                    { "response_code", response.StatusCode.ToString() },
+                    { "content_length", response.Content.Headers.ContentLength },
+                    { "content", Convert.ToBase64String(deviceLicense) }
+                });
 
             //activation bytes are at beginning of activation blob
             uint actBytes = BitConverter.ToUInt32(deviceLicense, deviceLicense.Length - ACTIVATION_BLOB_SZ);
